53e9fadf699b016ef27c8680a1bfbe6b3122a389
Mario L <mario.leeb0303@gmx.at>
2026-02-02 23:31:05 +0100

    Trial reading data ESP32 <-> Raspi

--- esp32/src/main.cpp
+++ esp32/src/main.cpp
@@ -1,18 +1,13 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  // Serial2 uses GPIO 16 (RX) and 17 (TX) on most ESP32s
+  Serial2.begin(115200, SERIAL_8N1, 16, 17); 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  // Send a test message to the Pi every 2 seconds
+  Serial2.println("EV_STATUS:READY");
+  delay(2000);
 }
